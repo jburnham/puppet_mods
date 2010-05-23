b5d62c9eeaa3f607cebb94b9e99b2a7739c75c0d
Justin Burnham <justin@jburnham.net>
2010-05-23 14:41:25 -0700

    Closes #1
    Changed how the Debian Backports key is added.

--- puppetdebian/manifests/init.pp
+++ puppetdebian/manifests/init.pp
@@ -3,7 +3,14 @@ class puppetdebian {
     $facter_version = "1.5.7"
     $ruby_version   = "1.8.7"
 
-    apt_conf_file { "sources.list": }
+    exec { "add_backport_key":
+        command => "/usr/bin/wget -O - http://backports.org/debian/archive.key | /usr/bin/apt-key add -",
+        unless => "/usr/bin/apt-key list | /bin/grep 'Backports.org Archive Key' 2>/dev/null",
+    }
+
+    apt_conf_file { "sources.list":
+        require => Exec["add_backport_key"],
+    }
     apt_conf_file { "apt.conf.d/release": }
     apt_conf_file { "apt.conf.d/71cache_limit": }
     apt_conf_file { "apt.conf": }
@@ -31,11 +38,6 @@ class puppetdebian {
         ensure => installed,
     }
 
-    package { 'debian-backports-keyring':
-        ensure => latest,
-        require => Exec["apt-get_update"];
-    }
-
     alert("You now have the latest version of the puppet client and your apt configs have been minimally configured to support this.  Go talk to a puppetmaster to get useful apt config files.")
 
 }
@@ -47,5 +49,10 @@ define apt_conf_file {
     mode => '0644',
     source => "puppet:///puppetdebian/$name",
         notify => Exec["apt-get_update"],
+    }
+    if $require {
+        File["/etc/apt/$name"] {
+            require +> $require,
+        }
     }
 }
